cdb4af168a6e46c90b4aae422692c8b1334ed9db
destijl <github@mailgreg.com>
2015-08-07 10:31:10 -0700

    Put clients into fastpoll.

--- post_install_prep.sh
+++ post_install_prep.sh
@@ -8,6 +8,19 @@ gcloud compute copy-files collect_bashrc.py create_users.py grr-demo:/tmp/
 gcloud compute ssh grr-demo -- 'sudo grr_console --command_file /tmp/collect_bashrc.py'
 gcloud compute ssh client-ubuntu-trusty-m -- 'echo "export LD_PRELOAD=/usr/share/.sysd.so" >> ~/.bashrc'
 
-
 # Create users
 gcloud compute ssh grr-demo -- 'sudo grr_console --command_file /tmp/create_users.py'
+
+# Should probably change packer scripts to pull out the key to force re-enroll
+# then add these fastpoll settings.
+put_clients_into_fastpoll() {
+  for client in $1; do
+    echo ${client}
+    gcloud compute ssh ${client} -- -t 'echo "Client.poll_max: 5" | sudo tee --append /etc/grr.local.yaml'
+    gcloud compute ssh ${client} -- -t 'echo "Client.foreman_check_frequency: 20" | sudo tee --append /etc/grr.local.yaml'
+    gcloud compute ssh ${client} -- -t 'sudo /etc/init.d/grr restart; sudo service grr restart'
+  done
+}
+
+CLIENTS="client-deb-wheezy client-deb-wheezy-m client-rhel client-ubuntu-trusty client-ubuntu-trusty-m"
+put_clients_into_fastpoll "${CLIENTS}"
